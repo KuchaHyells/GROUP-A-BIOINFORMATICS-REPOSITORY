069de4293cc1ebcc3a43a8a0465a509cc5b86671
Abdulzail <120309650+Abdulzail@users.noreply.github.com>
2024-02-16 23:41:20 +0100

    Update Simeon.java

--- Simeon.java
+++ Simeon.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Simeon {
     public static void main(String[] args) {
         System.out.println("Name: Simeon Benjamin");
         System.out.println("Email: simeonbenjamin5225@gmail.com");
