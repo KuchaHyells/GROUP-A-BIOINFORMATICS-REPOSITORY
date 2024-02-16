ec64811e3fe554e42576eb4cf562fef423f6effe
Abdulzail <120309650+Abdulzail@users.noreply.github.com>
2024-02-16 23:15:47 +0100

    Update main.r

--- main.r
+++ main.r
@@ -1,4 +1,10 @@
-print("Name: Yusuf Bhlyakamsu")
-print("Email: yusufbhlyakamsu@gmail.com")
-print("Slack Username: Yusuf Bhlyakamsu")
-print("Area of Interest: Data Processing")
+
+name <- "Yusuf Bhlyakamsu"
+email <- "yusufbhlyakamsu@gmail.com"
+slack_username <- "Yusuf Bhlyakamsu"
+area_of_interest <- "Data Processing"
+
+cat("Name: ", name, "\n", sep="")
+cat("Email: ", email, "\n", sep="")
+cat("Slack Username: ", slack_username, "\n", sep="")
+cat("Area of Interest: ", area_of_interest, "\n", sep="")
