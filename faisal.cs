a1095ca64e38872cbacbcdea00f421913e8be255
Abdulzail <120309650+Abdulzail@users.noreply.github.com>
2024-02-17 00:25:58 +0100

    Update faisal.cs

--- faisal.cs
+++ faisal.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Name: Faisal Ibrahim");
-        Console.WriteLine("email: ibrahimfaisal2001ha@gmail.com");
+        Console.WriteLine("email: ibrahimfaisal20001ha@gmail.com");
         Console.WriteLine("Slack Username: Faisal Ibrahim");
-        Console.WriteLine("Area of interest: megaoemics");
+        Console.WriteLine("Area of interest: Epigenomics");
     }
 }
 
