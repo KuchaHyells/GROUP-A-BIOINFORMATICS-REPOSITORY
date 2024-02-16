e896a21d76e33bef673d5ddcd0b540eb2efc974e
Abdulzail <120309650+Abdulzail@users.noreply.github.com>
2024-02-16 22:48:56 +0100

    zailani's commit

---
+++ zailani.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Name: Abdulkadir Zailani\n");
+    printf("Email: abdulkadirzailani116@gmail.com\n");
+    printf("Slack username: Abdulkadir Zailani\n");
+    printf("Area of Interest in Bioinformatics: Transoemics and data analyis\n");
+    
+    return 0;
+}
