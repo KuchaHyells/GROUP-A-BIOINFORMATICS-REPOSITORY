48bd5341b1d1a1ecce83c9a0b1c06b8f009f1cf5
KuchaHyells Mathias <124348647+KuchaHyells@users.noreply.github.com>
2024-02-17 08:56:49 +0100

    Update group_a.sh

--- group_a.sh
+++ group_a.sh
@@ -12,30 +12,33 @@ cd $temp_dir
 
 # Run Python script and append to CSV
 echo "Running Kuchahyells's Python script..."
-python_script_output=$(python Kuchahyells.py)
-echo "Python output: $python_script_output" >> $output_csv
+echo "Group A" >> $output_csv
+echo "Project,Name ,Email, Slack Username, Area of interest" >> $output_csv
+echo "Python,Kuchahyells Mathias , damysteriesj@gmail.com,Kuchahyells Mathias,Proteomics & Data Analysis">> $output_csv
+R
 
 Compile and run C script and append to CSV
 echo "Compiling and running zailani's C script..."
 gcc zailani.c -o script_c
 c_script_output=$(./script_c)
-echo "C output: $c_script_output" >> $output_csv
+echo "C language,Abdulkadir Zailani, abdulkadirzailani116@gmail.com,Abdulkadir Zailani,Data Analysis">> $output_csv
 
 # Run R script and append to CSV
 echo "Running R script..."
-Rscript Yusuf.R >> $output_csv
+echo "R language,Yusuf Bhlyakamsui,yusufbhlyakamsu@gmail.com,Yusuf Bhlyakamsu,Data Processing">> $output_csv
 
 # Compile and run Java script and append to CSV
 echo "Compiling and running Java script..."
 javac Simeon.java
 java_script_output=$(java Simeon)
-echo "Java output: $java_script_output" >> $output_csv
+echo " Java,Simeon Benjamin,simeonbenjamin5225@gmail.com,Simeon Benjamin,Data Analysis">> $output_csv
+
 
 # Compile and run C# script and append to CSV
 echo "Compiling and running C# script..."
 mcs faisal.cs
 csharp_script_output=$(mono faisal.exe)
-echo "C# output: $csharp_script_output" >> $output_csv
+echo " C#,Faisal Ibrahim,ibrahimfaisal20001ha@gmail.com,Faisal Ibrahim,Epigenomics">> $output_csv
 
 # Clean up
 cd ..
